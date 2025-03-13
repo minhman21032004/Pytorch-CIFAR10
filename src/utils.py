c985df8842f20dfe8a9741a03685b6bca29d57d6
minhman21032004 <phamminhman19052018@gmail.com>
2025-03-13 21:19:43 +0700

    Init

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def to_numpy(tensor):
+    image = tensor.cpu().clone().detach().numpy()
+    image = image.transpose(1,2,0)
+    image = image * np.array((0.5, 0.5, 0.5)) + np.array((0.5, 0.5, 0.5))
+    image = image.clip(0, 1)
+    return image
+
+def show_img(img_2d, title = ''):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(img_2d)
+    plt.title(title)
+    plt.show()
+
+
+
